6e137fdd5bb64acb2472d8bc7d295986361f19c3
Ankush <128672958+uic19bca1130@users.noreply.github.com>
2023-04-27 11:35:29 +0530

    Add project files.

---
+++ QuizAPi/Models/QuizDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizAPI.Models;
+using System.Security.Principal;
+
+namespace QuizAPi.Models
+{
+    public class QuizDbContext:DbContext
+    {
+        public QuizDbContext(DbContextOptions<QuizDbContext> options):base(options)// (ctor) parameters of the type DbContextOptions
+        { }
+
+        public DbSet <Question> Questions{ get; set; }
+        public DbSet<Participant>Participants  { get; set; }
+    }
+}
+ 
